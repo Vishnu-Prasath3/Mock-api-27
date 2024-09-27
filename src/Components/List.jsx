887ff5043d61f73855aee857a78e0d3fe6f2cc7b
vishnuprasath03 <eeevishnuprasath@gmail.com>
2024-09-27 20:06:12 +0530

    chromebook

--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react'
-import axios from 'axios'
 function List() {
   const[state,setstate]=useState([]);
-  let ID=()=>{
-    let data=document.getElementById("input-id");
-    let array=[];
-    
-    array=array.push(data.value);
-    
-    data.value="";
-  }
+const handlechange=(e)=>{
+console.log(e.target.value);
+}
+
+const handlesubmit=(e)=>{
+console.log(e);
+
+}
+
     return (
     <div>
 <div className="card" style={{width: "18rem"}}>
-  {/* <img src="..." className="card-img-top" alt="..."></img> */}
   <div className="card-body">
     <h5 className="card-title">ToDo-List</h5>
-    <input type='text' id='input-id'></input>
-    {/* <button >Add list</button> */}
-    <a href="#" onClick={()=>{ID()}} type='submit' className="btn btn-primary">Go somewhere</a>
+ <form onSubmit={ (e) => {
+  handlesubmit(e)
+ }}>
+ <input onChange={(e)=>{handlechange(e)}}    type='text' id='input-id'></input>
+ <button type='button' onSubmit={(a)=>{handlesubmit(a)}}>Submit</button>
+ </form>
   </div>
 </div>
     </div>
